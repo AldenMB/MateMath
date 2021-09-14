184b2ffd19992f07418753175838edf44055d01b
Alden Bradford <aldenbradford@gmail.com>
2021-09-14 18:39:49 -0400

    added style to the webpage

---
+++ main.js
@@ -0,0 +1,29 @@
+const ENGLISH = 'eng';
+const SPANISH = 'esp';
+
+window.onload = function() {
+	const sortbutton = document.getElementById('sort');
+	function get_primary_language(){
+		return (
+			(sortbutton.innerText === 'Mate ↔ Math') 
+			?
+			SPANISH
+			:
+			ENGLISH
+		);
+	};
+	
+	sortbutton.onclick = function(){
+		const english_first = get_primary_language() === SPANISH;
+		sortbutton.innerText = english_first ? 'Math ↔ Mate' : 'Mate ↔ Math';
+		for(let div of document.getElementsByClassName('twocol')){
+			const eng = div.querySelector('.english');
+			if((div.children[0] === eng) !== english_first){
+				//swap 'em
+				const second = div.children[1];
+				div.removeChild(second);
+				div.insertBefore(second, div.children[0]);
+			}
+		}
+	};
+};
